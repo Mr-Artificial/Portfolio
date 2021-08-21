a88ff256d6f46d0605fb441d769a32fef3ac2477
maxjack02 <maxim.jackson@live.com>
2021-08-21 07:37:48 -0700

    Developing Portfolio.

--- jsmain.js
+++ jsmain.js
@@ -1,1 +1,26 @@
+//Event Listener to respond to user clicks on hamburger
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-items");
+const navLink = document.querySelectorAll(".nav-item");
 
+const mobileMenu = () => {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+}
+
+const closeMenu = () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}
+
+hamburger.addEventListener("click", mobileMenu);
+navLink.forEach(n => n.addEventListener("click", closeMenu));
+
+//Smooth Scrolling JS from NavBar click
+$('nav-link').click(function (event) {
+    event.preventDefault();
+
+    $('html, body').animate({
+        scrollTop: $("#elementtoScrollToID").offset().top
+    }, 2000);
+});
